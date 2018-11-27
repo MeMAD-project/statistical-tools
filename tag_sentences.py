a2947aa2a6a563ed7f9aa7f1e9c1a4cddac5ca09
Arturs Polis <arturs.polis@helsinki.fi>
2018-11-27 15:55:00 +0200

    cleaned up sentence tagger

--- tag_sentences.py
+++ tag_sentences.py
@@ -9,6 +9,14 @@ from itertools import chain
 import random
 import argparse
 import sys
+import os
+
+
+# Define Stanford POS Tagger as a global variable:
+tagger = StanfordPOSTagger(
+    'stanford-postagger-2017-06-09/models/english-bidirectional-distsim.tagger',
+    path_to_jar='stanford-postagger-2017-06-09/stanford-postagger.jar',
+    java_options='-mx4000m')
 
 
 def collect_sentences_vg(data):
@@ -40,12 +48,15 @@ def collect_sentences_vist_dii(data):
 
 
 def tag_sentences(sentences):
-    sentences_tagged = st.tag_sents(word_tokenize(sent) for sent in sentences)
+    global tagger
+    sentences_tagged = tagger.tag_sents(word_tokenize(sent) for sent in sentences)
 
     return sentences_tagged
 
 
 def tag_sentences_in_batch(batch):
+    global tagger
+
     num_sentences = len(sentences)
 
     batch_size = math.floor(num_sentences / num_batches)
@@ -57,54 +68,58 @@ def tag_sentences_in_batch(batch):
     else:
         sentences_to_tag = sentences[batch_size * batch:batch_size * batch + batch_size]
 
-    tagged_sentences = tag_sentences(sentences_to_tag)
+    tagged_sentences = tag_sentences(tagger, sentences_to_tag)
 
     print("Finished batch {}".format(batch))
 
     return tagged_sentences
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', type=str, default='vg-regions',
-                        help='Dataset from which to extract tagged sentences')
+def main(args):
+    if args.dataset is None:
+        print("ERROR: No dataset specified. \n"
+              "HINT: Please specify dataset for example: --dataset vg-regions")
+        sys.exit(1)
 
-    args = parser.parse_args()
+    if args.input_file is None or not os.path.isfile(args.input_file):
+        print('ERROR: Invalid input file. Please make sure you specify a valid input file '
+              'corresponding to the dataset. \n'
+              'HINT: Use --input_file /path/to/captions-file.json')
+        sys.exit(1)
+
+    if args.output_file is None:
+        print('ERROR: No output file specified. '
+              'Please make sure you specify a valid output file.\n'
+              'HINT: Use --output_file /path/to/tagged_captions.txt')
+        sys.exit(1)
+
+    if os.path.isfile(args.output_file):
+        print('ERROR: File {} already exists. '
+              'Please rename or delete it'.format(args.output_file))
+        sys.exit(1)
+
+    global sentences
+    global num_batches
 
     print("Loading tagger...")
-    st = StanfordPOSTagger('stanford-postagger-2017-06-09/models/english-bidirectional-distsim.tagger',
-                           path_to_jar='stanford-postagger-2017-06-09/stanford-postagger.jar',
-                           java_options='-mx4000m')
 
     if args.dataset == 'vg-regions':
-        json_file = '/scratch/cs/imagedb/picsom/databases/visualgenome/download/' + \
-                    '1.2/VG/1.2/region_descriptions.json'
-        data = va.load_data(json_file)
-        assert len(data) == 108077
-        print("Collecting all sentences...")
-        sentences = collect_sentences_vg(data)
-        print('{} sentences collected'.format(len(sentences)))
-        pickle_path = 'pickles/vg_sentences_tagged.pkl'
+        collect_sentences = collect_sentences_vg
+        raw_data = va.load_json(args.input_file)
     elif args.dataset == 'coco':
-        json_file = '/scratch/cs/imagedb/picsom/databases/COCO/download/' + \
-                    'annotations/captions_train2014.json'
-        data = va.load_data(json_file)
-        # assert len(data) == 108077
-        print("Collecting all sentences...")
-        sentences = collect_sentences_coco(data)
-        print('{} sentences collected'.format(len(sentences)))
-        pickle_path = 'pickles/coco_sentences_tagged.pkl'
+        collect_sentences = collect_sentences_coco
+        raw_data = va.load_json(args.input_file)
     elif args.dataset == 'vist-dii':
-        json_file = '/scratch/cs/imagedb/picsom/databases/vist/download/' + \
-                    'data/dii/train.description-in-isolation.json'
-        data = va.load_data(json_file)
-        print("Collecting all sentences...")
-        sentences = collect_sentences_vist_dii(data)
-        print('{} sentences collected'.format(len(sentences)))
-        pickle_path = 'pickles/vist_dii_sentences_tagged.pkl'
+        collect_sentences = collect_sentences_vist_dii
+        raw_data = va.load_json(args.input_file)
     else:
-        print("Invalid dataset")
+        print("Invalid dataset {}".format(args.dataset))
         sys.exit(1)
+
+    print("Collecting all sentences...")
+    sentences = collect_sentences(raw_data)
+    print('{} sentences collected from dataset {} load from {}'.format(
+        len(sentences), args.dataset, args.input_file))
 
     random_indices = [random.randint(0, len(sentences)) for i in range(5)]
 
@@ -115,7 +130,7 @@ if __name__ == "__main__":
     num_batches = 15
     tasks = [x for x in range(num_batches)]
     # Number of threads to use:
-    num_workers = 5
+    num_workers = 4
 
     print("Starting tagging, using {} batches and {} workers"
           .format(num_batches, num_workers))
@@ -131,5 +146,20 @@ if __name__ == "__main__":
     for i, idx in enumerate(random_indices):
         print('{}: {}'.format(i + 1, sentences_tagged[idx]))
 
-    with open(pickle_path, 'wb') as f:
+    with open(args.output_path, 'wb') as f:
         pickle.dump(sentences_tagged, f)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str,
+                        help='Dataset from which to extract tagged sentences. '
+                        'Possible values: vg-regions, coco, vist-dii')
+    parser.add_argument('--input_file', type=str,
+                        help='File containing the text that we want tagged')
+    parser.add_argument('--output_file', type=str,
+                        help='Output file containing tagged sentences')
+
+    args = parser.parse_args()
+
+    main(args)
