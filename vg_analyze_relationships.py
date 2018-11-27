67d65e925f89337fbc252e5d40ec3ff55b3bcd65
Arturs Polis <arturs.polis@helsinki.fi>
2018-11-27 13:50:21 +0200

    fixed VG relationship analysis to be runnable again

--- vg_analyze_relationships.py
+++ vg_analyze_relationships.py
@@ -1,32 +1,80 @@
 import sys
 import analysis_funs as va
+import argparse
 
 
-data = va.load_data('relationships.json')
+def main(args):
+    print("Loading relationships data from: {}".format(args.relationships_json))
+    data = va.load_json(args.relationships_json)
 
-assert len(data) == 108077
+    assert len(data) == 108077
 
-if sys.argv[1] == '--help':
-	print("Usage:\n--rel_counts : count different types of relationships\n--summary_stats : print summary statistics")
+    print("=" * 80)
 
-if sys.argv[1] == '--rel_counts':
-	# Result tuple has the following order:
-	# r[rel_name_cnt, rel_syn_cnt], [subj_name_cnt, subj_syn_cnt], [obj_name_cnt, obj_syn_cnt]
+    if args.rel_counts:
+        print("Count all relationships (verbs and non-verbs):")
+        print("=" * 80)
+        rels, subjs, objs = va.count_relationships(data, va.human_synsets)
 
-	names = ['name','synset']
+        va.plot_and_output_csv(rels, ['relationship name', 'relationship synset'], 40,
+                               "Relationships with people as subjects",
+                               'relationships/rel_subj_people', batch=True)
 
-	rels, subjs, objs = va.count_relationships(data, va.human_synsets)
+        va.plot_and_output_csv(subjs, ['subject name', ' subject synset'], 40,
+                               "Subjects with people as subjects",
+                               'relationships/subj_subj_people', batch=True)
 
-	va.plot_and_output_csv(rels, names, 40, f"Relations with people as subjects", 'relationships/rel_subj_people' , batch = True) 
-	va.plot_and_output_csv(subjs, names, 40, f"Subjects with people as subjects", 'relationships/subj_subj_people' , batch = True) 
-	va.plot_and_output_csv(objs, names, 40, f"Objects with people as subjects", 'relationships/obj_subj_people' , batch = True) 
+        va.plot_and_output_csv(objs, ['object name', 'object synset'], 40,
+                               "Objects with people as subjects",
+                               'relationships/obj_subj_people', batch=True)
 
-	rels, subjs, objs = va.count_relationships(data, va.human_synsets, verbs = True)
+        print("=" * 80)
+        print("Count verb-only relationships:")
+        print("=" * 80)
+        rels, subjs, objs = va.count_relationships(data, va.human_synsets, verbs=True)
 
-	va.plot_and_output_csv(rels, names, 40, f"Relations with people as subjects, verbs only", 'relationships/rel_subj_people_verbs' , batch = True) 
-	va.plot_and_output_csv(subjs, names, 40, f"Subjects with people as subjects, verbs only", 'relationships/subj_subj_people_verbs' , batch = True) 
-	va.plot_and_output_csv(objs, names, 40, f"Objects with people as subjects, verbs only", 'relationships/obj_subj_people_verbs' , batch = True) 
+        va.plot_and_output_csv(rels, ['relationship name', 'relationship synset'], 40,
+                               "Relationships with people as subjects, verbs only",
+                               'relationships/rel_subj_people_verbs', batch=True)
 
-if sys.argv[1] == '--summary_stats':
+        va.plot_and_output_csv(subjs, ['subject name', ' subject synset'], 40,
+                               "Subjects with people as subjects, verbs only",
+                               'relationships/subj_subj_people_verbs', batch=True)
 
-	count, indices = stats_on_humans_in_relationships(data)
+        va.plot_and_output_csv(objs, ['object name', 'object synset'], 40,
+                               "Objects with people as subjects, verbs only",
+                               'relationships/obj_subj_people_verbs', batch=True)
+
+    print("=" * 80)
+    counts, indices = va.stats_on_humans_in_relationships(data)
+    print("=" * 80)
+    print("Plotting venn diagrams...")
+    print("=" * 80)
+
+    va.plot_venn(counts['rels']['all'], ['Human subjects', 'Human objects', 'Other'],
+                 'Relationships with humans as subjects or objects',
+                 filename='relationships/venn_rels_all', batch=True)
+
+    va.plot_venn(counts['rels']['verbs'], ['Human subjects', 'Human objects', 'Other'],
+                 "Relationships with humans as subjects or objects,\nverbs only",
+                 filename='relationships/venn_rels_verbs', batch=True)
+
+    va.plot_venn(counts['imgs']['all'], ['Human subjects', 'Human objects', 'Other'],
+                 'Images with humans as subjects or objects',
+                 filename='relationships/venn_imgs_all', batch=True)
+    va.plot_venn(counts['imgs']['verbs'], ['Human subjects', 'Human objects', 'Other'],
+                 "Images with humans as subjects or objects,\nverbs only",
+                 filename='relationships/venn_imgs_verbs', batch=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--relationships_json', type=str,
+                        help='location of Visual Genome relationships JSON file')
+    parser.add_argument('--rel_counts', action='store_true',
+                        help='Count different types of relationships, '
+                        'otherwise show summary stats only')
+
+    args = parser.parse_args()
+
+    main(args=args)
