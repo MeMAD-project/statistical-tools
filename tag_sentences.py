86badfd3cf4c07c17df00ff6fccd217262cbcd48
Arturs Polis <arturs.polis@helsinki.fi>
2018-11-28 19:41:01 +0200

    Change tagger output from Pickle to JSON; minor cleanup

--- tag_sentences.py
+++ tag_sentences.py
@@ -1,22 +1,14 @@
-# Import own functions for image analysis:
-import analysis_funs as va
 from nltk import word_tokenize
 from nltk.tag import StanfordPOSTagger
 from multiprocessing import Pool
-import pickle
+import json
 import math
 from itertools import chain
 import random
 import argparse
 import sys
 import os
-
-
-# Define Stanford POS Tagger as a global variable:
-tagger = StanfordPOSTagger(
-    'stanford-postagger-2017-06-09/models/english-bidirectional-distsim.tagger',
-    path_to_jar='stanford-postagger-2017-06-09/stanford-postagger.jar',
-    java_options='-mx4000m')
+from datetime import datetime
 
 
 def collect_sentences_vg(data):
@@ -68,7 +60,7 @@ def tag_sentences_in_batch(batch):
     else:
         sentences_to_tag = sentences[batch_size * batch:batch_size * batch + batch_size]
 
-    tagged_sentences = tag_sentences(tagger, sentences_to_tag)
+    tagged_sentences = tag_sentences(sentences_to_tag)
 
     print("Finished batch {}".format(batch))
 
@@ -76,6 +68,10 @@ def tag_sentences_in_batch(batch):
 
 
 def main(args):
+    global tagger
+    global sentences
+    global num_batches
+
     if args.dataset is None:
         print("ERROR: No dataset specified. \n"
               "HINT: Please specify dataset for example: --dataset vg-regions")
@@ -98,45 +94,61 @@ def main(args):
               'Please rename or delete it'.format(args.output_file))
         sys.exit(1)
 
-    global sentences
-    global num_batches
-
-    print("Loading tagger...")
+    num_batches = args.num_batches
 
     if args.dataset == 'vg-regions':
         collect_sentences = collect_sentences_vg
-        raw_data = va.load_json(args.input_file)
     elif args.dataset == 'coco':
         collect_sentences = collect_sentences_coco
-        raw_data = va.load_json(args.input_file)
     elif args.dataset == 'vist-dii':
         collect_sentences = collect_sentences_vist_dii
-        raw_data = va.load_json(args.input_file)
     else:
         print("Invalid dataset {}".format(args.dataset))
         sys.exit(1)
 
+    print("Loading tagger...")
+    tagger = StanfordPOSTagger(
+        'stanford-postagger-2017-06-09/models/english-bidirectional-distsim.tagger',
+        path_to_jar='stanford-postagger-2017-06-09/stanford-postagger.jar',
+        java_options='-mx4000m')
+
+    print("Loading dataset {}".format(args.dataset))
+    with open(args.input_file) as f:
+        raw_data = json.load(f)
+
     print("Collecting all sentences...")
     sentences = collect_sentences(raw_data)
-    print('{} sentences collected from dataset {} load from {}'.format(
+
+    # used for debugging:
+    if args.max_sentences is not None and args.max_sentences < len(sentences):
+        sentences = sentences[:args.max_sentences]
+        print("WARNING: Tagging only first {} sentences.".format(args.max_sentences))
+
+    if num_batches > len(sentences):
+        print("WARNING: There are more batches than sentences. "
+              "Making the number of batches equal to the number of sentences.")
+        num_batches = len(sentences)
+
+    print('{} sentences collected from dataset {} loaded from {}'.format(
         len(sentences), args.dataset, args.input_file))
 
-    random_indices = [random.randint(0, len(sentences)) for i in range(5)]
+    random_indices = [random.randint(0, len(sentences) - 1) for i in range(5)]
 
     print("5 random raw sentences at indices: {}".format(random_indices))
     for i, idx in enumerate(random_indices):
         print('{}: {}'.format(i + 1, sentences[idx]))
 
-    num_batches = 15
-    tasks = [x for x in range(num_batches)]
-    # Number of threads to use:
-    num_workers = 4
+    begin = datetime.now()
+    print("Starting tagging at {}, using {} batches and {} workers".format(
+        begin, num_batches, args.num_workers))
 
-    print("Starting tagging, using {} batches and {} workers"
-          .format(num_batches, num_workers))
+    # Perform the actual tagging using multiple threads:
+    with Pool(args.num_workers) as p:
+        sentences_tagged = p.map(tag_sentences_in_batch, range(num_batches))
 
-    with Pool(num_workers) as p:
-        sentences_tagged = p.map(tag_sentences_in_batch, tasks)
+    end = datetime.now()
+
+    print('Tagging ended at {}. Total tagging time: {}.'.format(end, end - begin))
 
     sentences_tagged = list(chain(*sentences_tagged))
 
@@ -146,8 +158,13 @@ def main(args):
     for i, idx in enumerate(random_indices):
         print('{}: {}'.format(i + 1, sentences_tagged[idx]))
 
-    with open(args.output_path, 'wb') as f:
-        pickle.dump(sentences_tagged, f)
+    print("Storing tagged sentenes in {}".format(args.output_file))
+    path = os.path.dirname(args.output_file)
+    os.makedirs(path, exist_ok=True)
+    with open(args.output_file, 'w') as f:
+        json.dump(sentences_tagged, f)
+
+    print("DONE!")
 
 
 if __name__ == "__main__":
@@ -159,6 +176,12 @@ if __name__ == "__main__":
                         help='File containing the text that we want tagged')
     parser.add_argument('--output_file', type=str,
                         help='Output file containing tagged sentences')
+    parser.add_argument('--num_workers', type=int, default=2,
+                        help='Number of Java workers doing the tagging')
+    parser.add_argument('--num_batches', type=int, default=16,
+                        help='Number of batches to use - more batches - less memory needed')
+    parser.add_argument('--max_sentences', type=int,
+                        help='Set a maximum number of sentences to tag (used for debugging)')
 
     args = parser.parse_args()
 
