d1fee4a9ac74e030eb5e8ccb53d6d998ff197a9d
Arturs Polis <arturs.polis@helsinki.fi>
2018-11-29 17:13:17 +0200

    removed hard-coded paths for synset to categories matcher

--- match_synsets_to_categories.py
+++ match_synsets_to_categories.py
@@ -1,6 +1,7 @@
 import warnings
 import argparse
-from collections import namedtuple
+import json
+from pandas.io.json import json_normalize
 from categories import Categories
 import sys
 from nltk.corpus import wordnet as wn
@@ -11,8 +12,6 @@ import os
 warnings.filterwarnings(
     "ignore", message="numpy.dtype size changed, may indicate binary incompatibility. Expected 96, got 88")
 tqdm.pandas()
-
-DatasetConfig = namedtuple('DatasetConfig', 'captions, sentence_syns')
 
 
 def get_hypernyms(synset):
@@ -49,24 +48,30 @@ def get_category_synsets_for_row(phrase, category):
         return results
 
 
-def main(args):
-    dataset_config = {
-        'vg-regions': DatasetConfig('pickles/vg_region_descriptions.feather',
-                                    'pickles/vg_sentences_tagged_syns.pandas.pkl'),
-        'coco': DatasetConfig('pickles/coco_captions_train2014.feather',
-                              'pickles/coco_sentences_tagged_syns.pandas.pkl')
-    }
+def dataset_to_pandas(data_file, dataset):
+    """Accept raw JSON/TXT/other data that is then formatted into a same
+    Pandas data frame based on the dataset-specific rules"""
 
-    config = dataset_config.get(args.dataset)
-    if not config:
-        print('Invalid dataset specified')
+    if dataset == 'vg-regions':
+        with open(data_file) as f:
+            data = json.load(f)
+        # Creates Pandas data frame with 'regions' object as a root:
+        df = json_normalize(data, 'regions')
+    elif dataset == 'coco':
+        df = None
+    else:
+        print("ERROR: Unknown dataset: {}.".format(dataset))
         sys.exit(1)
 
-    print("Loading caption data from {}".format(config.captions))
-    df = pd.read_feather(config.captions)
+    return df
 
-    print("Loading sentence synsets from {}".format(config.sentence_syns))
-    sentence_syns = pd.read_pickle(config.sentence_syns)
+
+def main(args):
+    print("Loading {} caption data from {}".format(args.dataset, args.data_file))
+    df = dataset_to_pandas(args.data_file, args.dataset)
+
+    print("Loading sentence synsets from {}".format(args.synset_file))
+    sentence_syns = pd.read_json(args.synset_file, typ='series')
 
     print("Loading category '{}'".format(args.category))
     category = set(Categories[args.category])
@@ -78,23 +83,33 @@ def main(args):
     print("Adding results to master data")
     df[args.category] = category_synsets
 
-    file_name = os.path.basename(config.sentence_syns)
+    file_name = os.path.basename(args.synset_file)
     file_name_no_ext = os.path.splitext(file_name)[0]
-    output_file_name = '{}_{}.pkl'.format(file_name_no_ext, args.category)
-    output_path = os.path.join(args.output_path, output_file_name)
+    output_file_name_json = '{}_{}.json'.format(file_name_no_ext, args.category)
+    output_file_name_pandas = '{}_{}.pandas.pkl'.format(file_name_no_ext, args.category)
+    output_file_json = os.path.join(args.output_path, output_file_name_json)
+    output_file_pandas = os.path.join(args.output_path, output_file_name_pandas)
 
-    print("Saving results to {}".format(output_path))
-    df.to_pickle(output_path)
+    print("Saving results to {} for user analysis".format(output_file_json))
+    df.to_json(output_file_json, orient='records')
+    print("Saving results to {} for further steps in the pipeline.".format(output_file_pandas))
+    df.to_pickle(output_file_pandas)
+    print("DONE!")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, default='vg-regions',
                         help='Dataset to use')
+    parser.add_argument('--data_file', type=str,
+                        help='original dataset in JSON/TXT/other format')
+    parser.add_argument('--synset_file', type=str,
+                        help='JSON file containing synsets for the dataset. '
+                        'Should have the same number of entries as the dataset file.')
     parser.add_argument('--category', type=str, default='location',
                         help='Which category to do the stats for')
-    parser.add_argument('--output_path', type=str, default='pickles/',
-                        help='path where to save output images and csvs')
+    parser.add_argument('--output_path', type=str, default='output/',
+                        help='path where to save output data (both JSON and serialized Pandas')
 
     args = parser.parse_args()
 
