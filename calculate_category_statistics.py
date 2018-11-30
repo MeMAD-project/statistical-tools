0054984d024a8d8005d878b3a76e06ac9f120b0b
Arturs Polis <arturs.polis@helsinki.fi>
2018-11-30 13:46:08 +0200

    cleaned up and updated final stats output

--- calculate_category_statistics.py
+++ calculate_category_statistics.py
@@ -63,6 +63,11 @@ def process_data(df, category, group_by):
     print("Aggregating category results by {}".format(group_by))
     results = grouped.apply(lambda x: aggregate_category_in_group(x))
 
+    # counts is a nested list of length 3 containing following Counter objects:
+    # counts[0]: Counter for words that occured in captions
+    # counts[1]: -"- Synsets corresponding to the above words
+    # counts[2]: -"- Parent synsets that were matched to the actual category
+    # (location, temporal, etc)
     counts = get_counts(results)
 
     return counts, n_entries
@@ -71,22 +76,68 @@ def process_data(df, category, group_by):
 def main(args):
     # Load data frame with categories matched
     df = pd.read_pickle(args.data_file)
-    word_cnt, synset_cnt, category_cnt = process_data(df, args.category, args.groupby)
-    va.plot_bar_counts(word_cnt)
+    counts, n_unique_images = process_data(df, args.category, args.group_by)
+
+    print("-" * 80)
+
+    print("Synsets that belong to category {} found in {} unique images".format(
+        args.category, n_unique_images))
+
+    print("=" * 80)
+
+    va.plot_and_output_csv(counters=[counts[0]],
+                           names=['words'], maxnum=45,
+                           title='{} {} - Words'.format(
+                               args.output_name, args.category),
+                           filename_base='{}_{}'.format(args.output_prefix,
+                                                        args.category),
+                           batch=True)
+
+    print("=" * 80)
+
+    va.plot_and_output_csv(counters=[counts[1]],
+                           names=['synsets'], maxnum=45,
+                           title='{} {} - Synsets'.format(
+                               args.output_name, args.category),
+                           filename_base='{}_{}'.format(args.output_prefix,
+                                                        args.category),
+                           batch=True)
+
+    print("=" * 80)
+
+    va.plot_and_output_csv(counters=[counts[2]],
+                           names=['category_synsets'], maxnum=45,
+                           title='{} {} - Category Synsets'.format(args.output_name,
+                                                                   args.category),
+                           filename_base='{}_{}'.format(args.output_prefix,
+                                                        args.category),
+                           batch=True)
 
 
-# TODO supply names for plots!
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('data_file', nargs='?', type=str, default='',
                         help='Data file to load')
+    parser.add_argument('--output_name', type=str,
+                        help='Name to use in the output images, '
+                        'should be dataset specific.')
+    parser.add_argument('--output_prefix', type=str,
+                        help='prefix to put in front of output files, '
+                        'should be dataset specific')
     parser.add_argument('--category', type=str, default='location',
                         help='Name of the field we are interested in')
     parser.add_argument('--group_by', type=str, default='image_id',
                         help='name of the column to group by - '
                              'we usually want data grouped by images')
-    parser.add_argument('--output_path', type=str, default='results/',
-                        help='path where to save output images and csvs')
+    # NOTE:
+    # The below --output_path parameter is not currently in use. In order to enable it
+    # analysis_funs.py needs to be modified so that all functions that perform
+    # plotting and storing to CSV take one optional parameter output_path,
+    # which is then used as a prefix where all the files are stored to.
+    # The above functions all take parameter "filename", so it's easy to find them!
+
+    # parser.add_argument('--output_path', type=str, default='results/',
+    #                   help='path where to save output images and csvs')
 
     args = parser.parse_args()
 
